caaa9fae0bdc7efe2f6a408475c4f7a1d1fb826d
dkokonkwo <d.okonkwo@alustudent.com>
2024-07-31 16:59:00 -0300

    system performance

---
+++ q1_sorting_names/functions.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/* callback function for sorting based on order */
+int sort_names(int (*sort)(char *[]), char *names[])
+{
+sort(char *names[]);
+return (0);
+}
+
+/* counts num of names in array */
+int count_names(char *names[])
+{
+int count = 0;
+while (*names)
+{
+i++;
+names++;
+}
+return (count);
+}
+
+
